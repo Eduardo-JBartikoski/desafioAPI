945f388fbffa632e619e5254f386722011b4361e
Eduardo Bartikoski <eduardob.i207@gmail.com>
2025-06-30 16:49:14 -0300

    Update TransactionController.java

--- desafio.springboot/src/main/java/desafio/controller/TransactionController.java
+++ desafio.springboot/src/main/java/desafio/controller/TransactionController.java
@@ -1,8 +1,16 @@
 package desafio.controller;
+import desafio.api.springboot.Transaction;
+import desafio.api.springboot.dto.TransactionRequest;
 import desafio.api.springboot.services.TransactionService;
+import jakarta.validation.Valid;
 
+import java.time.OffsetDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +25,21 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    public TransactionService getTransactionService() {
+        return transactionService;
+    }
+
     @PostMapping
+    public ResponseEntity<Void> createTransaction(@Valid @RequestBody TransactionRequest request){
+
+        if (request.getDataHora().isAfter(OffsetDateTime.now())) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        transactionService.addTransaction(new Transaction(request.getValor(), request.getDataHora()));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
   
 
 }
